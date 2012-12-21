e450d5735f973911f6e3a067e453edfebfc3eb49
bwma <bwma@thoughtworks.com>
2012-12-21 15:46:40 +0800

    solve the narcissus number issue

--- src/narcissus_number.c
+++ src/narcissus_number.c
@@ -1,11 +1,11 @@
-/*given the input is limited from 100 to 999, print out the number ABC = A*A + B*B + C*C. This is called the narcissus number */
+/*given the input is limited from 100 to 999, print out the number ABC = A*A*A + B*B*B*B + C*C*C. This is called the narcissus number */
 #include <stdio.h>
 
-int is_narcissus_number(int number) {
+int is_narcissus(int number) {
   int a = number / 100;
   int c = number % 10;
   int b = (number / 10) % 10;
-  if (number == (a * a + b * b + c * c))
+  if (number == (a * a * a + b * b * b + c * c * c))
     return 1;
   return 0;
 }
@@ -14,7 +14,7 @@ int main(){
   int number = 100;
   
   for (; number < 1000; number++){
-    if (is_narcissus_number(number) == 1)
+    if (is_narcissus(number) == 1)
       printf("%d is a narcissus number between 100 and 999.\n", number);
   }
   
