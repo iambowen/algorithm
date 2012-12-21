85a8d96e99cf09651cb6863b706ab0552a00358b
bwma <bwma@thoughtworks.com>
2012-12-21 14:27:33 +0800

    solve the count number issue

--- src/count_number_bits.c
+++ src/count_number_bits.c
@@ -4,28 +4,34 @@
 #define INF 1000000000
 int divider(int bits) {
    int number = 1;
-   for(int i = 1; i <= bits; ++i){
+   int i = 1;
+   for(; i <= bits; ++i){
     number *= 10;
   }
   return number;
 }
-int exit_if_number_is_too_large(int number) {
-   if number > INF
+
+void exit_if_number_is_too_large(long number) {
+   if (number > INF) {
      printf("Sorry, your input number is over the boarder.\n");
-   exit(666);  
+     exit(666);  
+   }
 }
+
 int main()
 {
-  int number;
+  long number;
   int count = 0;
-
-  scanf("%d", &number);
-  for(int i = 9; i > 0; i--) {
-    int divider = divider(i); 
-    exit_if_number_is_too_large(divider) 
-    if (number / divider >= 1)
+  int i = 9;
+  scanf("%ld", &number);
+  exit_if_number_is_too_large(number);
+  for(; i > 0; i--) {
+    long d= divider(i); 
+    if (number / d >= 1){
       count = i;
+      break;
+    }
   }  
-  printf("the number %d has %d bits", number, count);
+  printf("the number %ld has %d bits.\n", number, count + 1);
   return 0;
 }
