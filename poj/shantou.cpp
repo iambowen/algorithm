d97ace92eec0f10abc6c5803efb0facc3bc31f3f
Bowen Ma <bwma@CNbwma.lan>
2015-09-24 23:19:00 +0800

    more solutions

--- poj/shantou.cpp
+++ poj/shantou.cpp
@@ -7,29 +7,53 @@ int main(){
 
   cin >> m >> n;
 
-  char hills[m][n];
+  int hills[m][n];
 
   for(int i = 0; i < m; i++){
     for(int j = 0; j < n; j++){
       cin >> hills[i][j];
-      }
     }
+  }
+
   for(int i = 0; i < m; i++){
     for(int j = 0; j < n; j++){
+      int result = 0;
       if( j - 1 >= 0){
         if(hills[i][j] < hills[i][j - 1]){
           continue;
+        } else {
+          result = 1;
         }
       }
-      if()
-      if ((j - 1 >= 0 && hills[i][j] >= hills[i][j - 1])
-          && (i - 1 >= 0 && hills[i][j] >= hills[i - 1][j])
-          && (j + 1 < n && hills[i][j] >= hills[i][j + 1])
-          && (i + 1 < n && hills[i][j] >= hills[i + 1][j])
-        ) {
-          cout << i << " " << j << endl;
+      if( j + 1 < n){
+        if(hills[i][j] < hills[i][j + 1]){
+          continue;
+        } else {
+          result = 1;
+        }
+      }
+      if( i - 1 >= 0){
+        if(hills[i][j] < hills[i - 1][j]){
+          continue;
+        } else {
+          result = 1;
+        }
+      }
+
+      if( i + 1 < m){
+        if(hills[i][j] < hills[i + 1][j]){
+          continue;
+        } else {
+          result = 1;
+        }
+      }
+
+      if (result == 1) {
+        cout << i << " " << j << endl;
       }
     }
+  }
 
   return 0;
+
 }
