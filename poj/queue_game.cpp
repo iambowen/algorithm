a22649fa200927e7d385ec3048cebb8266eb38c2
Bowen Ma <iambowen.m@gmail.com>
2016-09-26 13:03:45 +0800

    complete the queue game

--- poj/queue_game.cpp
+++ poj/queue_game.cpp
@@ -1,32 +1,35 @@
+
+// http://pkuic.openjudge.cn/hw07/solution/6749945/
+
 #include <iostream>
 using namespace std;
 
-void queueGame(char queue[], int left, int right){
-  if (queue[right] == '\0' || left < 0){
-    return queue;
-  }
-  if(queue[left] == '(' && queue[right] == ')'){
-    cout << left << ' ' << right << endl;
-    queue[left] = '0';
-    queue[right] = '0';
-    if(queue[right + 1] == ')'){
-      queueGame(queue, left - 1, right + 1);
-    }else{
-      queueGame(queue, right + 1, right + 2);
-    }
-  } else if(queue[left] == '(' && queue[right] == '('){
-    queueGame(queue, left - 1, right);
-  } else if(queue[left] == '(' && queue[right] == '('){
-    queueGame(queue, right, right + 1);
+char man;
+
+int queueGame(char queue[], int pivotal){
+  int n;
+
+  if (queue[pivotal] != man){
+    return pivotal;
+  } else {
+    n = queueGame(queue, pivotal + 1);
+    cout << pivotal << " " << n << endl;
+    return queueGame(queue, n + 1);
   }
 }
 
 int main(){
-  char queue[100];
+  char queue[101] = {0};
+  int n;
 
-  cin.getline(queue, 100);
 
-  queueGame(queue, 0, 1);
+  cin >> queue;
+
+  man = queue[0];
+  
+  n = queueGame(queue, 1);
+
+  cout << 0 << " " << n << endl;
 
   return 0;
 }
